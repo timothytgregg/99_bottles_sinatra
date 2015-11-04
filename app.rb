8a15f12b3370fbde6eccd5cf4bc014b8b51cf202
Timothy Gregg <timtgregg@gmail.com>
2015-11-04 11:14:48 -0500

    more elegant solution

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/bob' do
-  return "bob"
+get '/' do
+  "99 bottles of beer on the wall. <a href='/98'>Take One Down.</a>"
 end
 
 get '/:num' do
-  if params[:num].to_i!=0
-    "#{params[:num]} bottles of beer on the wall.
-    <a href=/#{params[:num].to_i-1}>Take One Down</link>"
-  elsif params[:num].to_i==0
+  num=params[:num].to_i
+  if num==0
     "No more bottles of beer on the wall.
     <a href=/99>Start Over</link>"
+  else
+    "#{num} bottles of beer on the wall.
+    <a href=/#{num-1}>Take One Down.</link>"
   end
 end
